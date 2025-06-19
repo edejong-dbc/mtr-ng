9856e49a17e600e458bf6ab94b41598f780771f5
Edwin de Jong <edwin.de.jong@databuildcompany.com>
2025-06-20 00:57:02 +0200

    fix: Fix Homebrew formula installation method and SHA256 handling

--- Formula/mtr-ng.rb
+++ Formula/mtr-ng.rb
@@ -2,14 +2,15 @@ class MtrNg < Formula
   desc "Modern My Traceroute with real-time network path visualization"
   homepage "https://github.com/edejong-dbc/mtr-ng"
   url "https://github.com/edejong-dbc/mtr-ng/archive/v0.3.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000" # Will be updated automatically
+  # SHA256 will be calculated automatically by Homebrew
   license "MIT OR Apache-2.0"
   head "https://github.com/edejong-dbc/mtr-ng.git", branch: "master"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "build", "--release"
+    bin.install "target/release/mtr-ng"
     man1.install "install/mtr-ng.1"
   end
 
